e8d8e6db6d02d9f2cd4981d46fcbc1e4a59adc1a
Amy <lewia046@newschool.edu>
2026-06-12 10:38:18 -0400

    Adding copy to project pages, slight tweaks to homepage connect

--- assets/main.js
+++ assets/main.js
@@ -6,3 +6,19 @@ const setScrolled = () => {
 window.addEventListener("scroll", setScrolled, { passive: true });
 window.addEventListener("resize", setScrolled);
 setScrolled();
+
+const emailButton = document.getElementById("copy-email");
+
+emailButton.addEventListener("click", (event) => {
+	event.preventDefault();
+
+	navigator.clipboard.writeText("ae.lewis15@gmail.com");
+
+	emailButton.textContent = "Copied!";
+	emailButton.classList.add("copied");
+
+	setTimeout(() => {
+		emailButton.textContent = "Email";
+		emailButton.classList.remove("copied");
+	}, 2000);
+});
